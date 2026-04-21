50cd09ce3ac4e3adfa140a78484ba32b2e14cfa0
kim-se-hee <saree98@naver.com>
2026-04-21 20:44:12 +0900

    init:최초 커밋

---
+++ src/main/java/ksh/tryptoengine/TryptoEngineApplication.java
@@ -0,0 +1,11 @@
+package ksh.tryptoengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryptoEngineApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TryptoEngineApplication.class, args);
+    }
+}
